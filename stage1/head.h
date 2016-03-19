d9948eee909eab5beaf929adedf5558617a12157
sunshine0904 <sjl172270102@163.com>
2016-03-19 17:51:12 +0800

    add stage1 file

---
+++ stage1/head.h
@@ -0,0 +1,61 @@
+#ifndef __HEAD_H
+#define __HEAD_H
+#include <stdio.h>
+
+struct m_ip
+{
+	int ip_dec[4];
+	unsigned char ip_h[4];
+};
+
+struct path_index
+{
+	unsigned char index1:5;
+	unsigned char index2:5;
+	unsigned char index3:5;
+	unsigned char index4:5;
+	unsigned char index5:5;
+};
+
+struct path_as
+{
+	unsigned char as:5;
+};
+
+#if 1
+struct aes_encry
+{
+	unsigned char ip1_ip3_time[16];
+};
+#endif
+
+#pragma pack(1)
+struct encrypt_ip
+{
+	unsigned char session_num[4];
+	unsigned char len:5;
+	struct path_index pi;
+	struct path_as    pas[0];	
+	struct aes_encry aes[0];
+};
+#pragma pack()
+
+int get_count_ip(FILE *fp);
+void split_str(char *buf,struct m_ip stemp[]);
+int get_bit_of_ip(struct m_ip bit_temp,int num_bit);
+int get_flag_bit(struct m_ip src_ip[],int count_ip,int stor_flag_bit[]);
+#if 0
+//void dectobin(int num)
+int is_zero(int a,int b);
+int bit(struct m_ip bit_temp,int ip_nbit);
+int is_valid(struct m_ip is_temp[num_ip],int ip_nbit);
+
+unsigned int get_fir_colum(struct m_ip bit_temp[num_ip],int colum,short int a[num_ip][4]);
+unsigned int get_sec_colum(struct m_ip bit_temp[num_ip],int colum,short int a[num_ip][4],int fir_colum);
+unsigned int get_thir_colum(struct m_ip bit_temp[num_ip],int colum,short int a[num_ip][4],int fir_sec_colum);
+struct encrypt_ip stor_index_as(struct encrypt_ip ecy_ip,int fir,int sec,int thir,short int a[num_ip][4]);
+void check_right(struct m_ip is_temp[num_ip],int a,int b,int c);
+#endif
+
+
+#endif/*_HEAD_H*/
---
+++ stage1/stage1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "aes_lib.h"
+#include "head.h"
+
+int main()
+{
+	FILE *fps;
+	int count_ip = 0,i = 0,j = 0;
+	char buffer[25];
+
+	//open ip file
+	if((fps = fopen("ip.txt","r")) == NULL)
+	{
+		printf("open ip.txt failed\n");
+		return 0;
+	}
+	count_ip = get_count_ip(fps);
+	
+	int stor_flag_bit[count_ip];
+	//read ip.txt and storage in src_ip
+	struct m_ip src_ip[count_ip];
+	while(fgets(buffer,20,fps) != NULL)
+	{
+		//printf("%s",buffer);
+		split_str(buffer,src_ip);
+	}
+
+#if 1
+	//just for debug
+	for( i = 0;i<count_ip;i++)
+	{
+		printf("ip%d:   ",i+1);
+		for(j = 0;j<4;j++)
+		{
+			printf("%02x ",src_ip[i].ip_h[j]);
+		}
+		printf("\n");
+	}
+#endif	
+	get_flag_bit(src_ip,count_ip,stor_flag_bit);	
+
+	return 0;
+}
