4410b88ae375e7719ba345404db2eae47509189a
sunshine0904 <sunshineboylei@gmail.com>
2016-07-12 19:43:18 +0800

    add test_sha

---
+++ cmpexp0707/stage3/test_sha.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "sha1.h"
+
+//variable about hmac_sha
+unsigned char key[16] = {0x0};
+unsigned char key_len = 16;
+unsigned char data[16] = {0x0};
+unsigned char data_len = 16;
+unsigned char out[16] = {0x0};
+unsigned char out_len = 16;
+
+unsigned char ip1[4] = {0xc0,0xa8,0x1,0x1};
+unsigned char ip2[4] = {0xc0,0xa8,0x1,0x2};
+unsigned char ip3[4] = {0xc0,0xa8,0x1,0x3};
+unsigned char ip4[4] = {0xc0,0xa8,0x1,0x4};
+unsigned char ip5[4] = {0xc0,0xa8,0x1,0x5};
+
+void main()
+{
+	int i = 0,j = 0;	
+	//initital key
+	memset(key,0,16);
+	memcpy(key,ip1,4);
+	
+	//initital data
+	memset(data,0,16);
+
+	//initital out
+	memset(out,0,16);
+
+	//initital data
+	memset(data,0,16);
+	memcpy(data,ip2,4);
+	memcpy(data+4,ip3,4);
+	memcpy(data+8,ip4,4);
+	memcpy(data+12,ip5,4);
+
+	for(i = 0;i<5;i++)
+	{
+		hmac_sha(key,key_len,data,data_len,out,out_len);
+
+		printf("out %d:\n",out_len);
+		for(j = 0;j<out_len;j++)
+		{
+			printf("%02x ",out[j]);
+		}
+		printf("\n");
+		memset(out,0,16);
+	}
+
+}
